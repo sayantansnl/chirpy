6aa8d66ad2e1cc857fcfeb648993f43d4eba4942
sayantansnl <sayantansanyal72@gmail.com>
2025-12-04 13:38:13 +0000

    fileserver

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
+
 	mux := http.NewServeMux()
-	
-	server := &http.Server{
-		Addr: ":" + port,
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
 	}
-	log.Printf("Serving on port: %s", port)
-	log.Fatal(server.ListenAndServe())
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
