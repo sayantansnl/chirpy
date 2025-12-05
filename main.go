5efcb9a060595a72c86b22bc650f8ec524dc6ddc
sayantansnl <sayantansanyal72@gmail.com>
2025-12-05 07:17:41 +0000

    middleware added

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits  atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) numRequestsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	body := []byte(fmt.Sprintf("Hits: %d", int(cfg.fileserverHits.Load())))
+	if _, err := w.Write(body); err != nil {
+		http.Error(w, "error displaying number of requests", http.StatusInternalServerError)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := &apiConfig{}
+
+	handler := http.FileServer(http.Dir(filepathRoot))
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
 
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.numRequestsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
