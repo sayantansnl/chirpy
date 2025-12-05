ff1c3571ef0e267767d3cc4f91bb4dbe44836e86
sayantansnl <sayantansanyal72@gmail.com>
2025-12-05 05:57:13 +0000

    custom handler

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +21,14 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	bodyText := "OK"
+	if _, err := w.Write([]byte(bodyText)); err != nil {
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+	}
 }
