a4df942b342e3effe57a858ea90f9ba411eb3072
sayantansnl <sayantansanyal72@gmail.com>
2025-12-14 07:05:56 +0000

    able to filter chirps by author_id

--- getAllChirpsHandler.go
+++ getAllChirpsHandler.go
@@ -2,25 +2,55 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) getAllChirpsHandler(w http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.queries.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "unable to get chirps", err)
+	authorID := r.URL.Query().Get("author_id")
+
+	if authorID == "" {
+		chirps, err := cfg.queries.GetAllChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "unable to get chirps", err)
+		}
+
+		chirpsArray := []Chirp{}
+
+		for _, chirp := range chirps {
+			chirpsArray = append(chirpsArray, Chirp{
+				ID: chirp.ID,
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body: chirp.Body,
+				UserID: chirp.UserID,
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, chirpsArray)
+	} else {
+		userID, err := uuid.Parse(authorID)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "unable to parse into uuid.UUID", err)
+			return
+		} 
+		chirps, err := cfg.queries.GetAllChirpsByUserID(r.Context(), userID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "unable to get chirps", err)
+		}
+
+		chirpsArray := []Chirp{}
+
+		for _, chirp := range chirps {
+			chirpsArray = append(chirpsArray, Chirp{
+				ID: chirp.ID,
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body: chirp.Body,
+				UserID: chirp.UserID,
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, chirpsArray)
 	}
-
-	chirpsArray := []Chirp{}
-
-	for _, chirp := range chirps {
-		chirpsArray = append(chirpsArray, Chirp{
-			ID: chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body: chirp.Body,
-			UserID: chirp.UserID,
-		})
-	}
-
-	respondWithJSON(w, http.StatusOK, chirpsArray)
 }
